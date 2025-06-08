8e31aeae06f1e3e181d9b5b29fc076d535566f23
Briandy23 <21520694@gm.uit.edu.vn>
2025-06-08 14:44:04 +0700

    fix metric map

--- predict_kgat.py
+++ predict_kgat.py
@@ -244,7 +244,7 @@ def predict(args):
     k_min = min(Ks)
     k_max = max(Ks)
 
-    cf_scores,metrics_dict = evaluate(model, data, Ks, device)
+    metrics_dict = evaluate(model, data, Ks, device)
     print('CF Evaluation: Precision [{:.4f}, {:.4f}], Recall [{:.4f}, {:.4f}], NDCG [{:.4f}, {:.4f}], F1 [{:.4f}, {:.4f}], MAP [{:.4f}, {:.4f}]'.format(
         metrics_dict[k_min]['precision'], metrics_dict[k_max]['precision'], 
         metrics_dict[k_min]['recall'], metrics_dict[k_max]['recall'], 
